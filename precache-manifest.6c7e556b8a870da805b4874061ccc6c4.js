9ad6e31d99e7db18aa1b197091d7d5c64c040acb
Julian Boyko <julian.boyko@outlook.com>
2020-08-04 19:04:15 -0400

    Updates

---
+++ precache-manifest.6c7e556b8a870da805b4874061ccc6c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab4ebfae31011990d56d23a7400d233",
+    "url": "/BTH745/index.html"
+  },
+  {
+    "revision": "974d022b1691263b3844",
+    "url": "/BTH745/static/css/2.24d4744e.chunk.css"
+  },
+  {
+    "revision": "f9e6d0db1719908098f3",
+    "url": "/BTH745/static/css/main.1af558d4.chunk.css"
+  },
+  {
+    "revision": "974d022b1691263b3844",
+    "url": "/BTH745/static/js/2.aa78ad61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BTH745/static/js/2.aa78ad61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e6d0db1719908098f3",
+    "url": "/BTH745/static/js/main.664850b4.chunk.js"
+  },
+  {
+    "revision": "2689f69cf66b24043094",
+    "url": "/BTH745/static/js/runtime-main.1cb0d9be.js"
+  }
+]);
